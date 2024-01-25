ae8d47e813859bd8544019058bb2970d2e38707e
eyorokumo <eyorokumodicksonemmanuel@gmail.com>
2024-01-25 11:54:05 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let menuBtn = document.querySelector("#menu_btn");
+let navbar = document.querySelector(".header .flex .navbar");
+
+menuBtn.onlick = () => {
+  menuBtn.classList.toggle("fa-times");
+};
